c5e2506486a627228cdd8bd168fbf4e9cd471181
Aerossky <riskyradeonrx560@gmail.com>
2023-04-17 18:50:13 +0700

    chapter 1 section 4

---
+++ Risky-0706012110052-AFL3/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Risky-0706012110052-AFL3
+//
+//  Created by MacBook Pro on 17/04/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
