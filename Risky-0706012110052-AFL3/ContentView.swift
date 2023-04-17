944640347a53f6221e2fafafc9dd0dc7710058fa
Aerossky <riskyradeonrx560@gmail.com>
2023-04-17 18:47:35 +0700

    chapter 1 section 2

--- Risky-0706012110052-AFL3/ContentView.swift
+++ Risky-0706012110052-AFL3/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello SwiftUI!")
-            .padding()
+        Text("Turtle Rock")
+            .font(.title)
     }
 }
 
