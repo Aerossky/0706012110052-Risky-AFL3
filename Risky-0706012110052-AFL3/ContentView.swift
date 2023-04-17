901519017cdfa0c76c6e30bd6f37de9e5d08433f
Aerossky <riskyradeonrx560@gmail.com>
2023-04-17 18:46:24 +0700

    Initial Commit

---
+++ Risky-0706012110052-AFL3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Risky-0706012110052-AFL3
+//
+//  Created by MacBook Pro on 17/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Risky-0706012110052-AFL3/Risky_0706012110052_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  Risky_0706012110052_AFL3App.swift
+//  Risky-0706012110052-AFL3
+//
+//  Created by MacBook Pro on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Risky_0706012110052_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
