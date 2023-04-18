d959fc02c80d31650cf2b1b1bfe1c39ef4a124e7
Aerossky <riskyradeonrx560@gmail.com>
2023-04-18 08:29:09 +0700

    chapter 2 section 2

--- Risky-0706012110052-AFL3/Landmark.swift
+++ Risky-0706012110052-AFL3/Landmark.swift
@@ -15,13 +15,14 @@ struct Landmark: Hashable, Codable {
     var park: String
     var state: String
     var description: String
-
     private var imageName: String
+    
     var image: Image {
         Image(imageName)
     }
 
     private var coordinates: Coordinates
+    
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
---
+++ Risky-0706012110052-AFL3/Views/LandmarkRow.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkRow.swift
+//  Risky-0706012110052-AFL3
+//
+//  Created by MacBook Pro on 18/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+//    inisialisasi struct landmark
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack{
+            landmark.image
+            .resizable()
+            .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
