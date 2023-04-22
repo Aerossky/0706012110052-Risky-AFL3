6a2763ca9a4b64fe55a21196aa2b73608b36929c
Aerossky <riskyradeonrx560@gmail.com>
2023-04-22 14:13:46 +0700

    chapter 4 section 4

---
+++ HexagonParameters.swift
@@ -0,0 +1,55 @@
+//
+//  HexagonParameters.swift
+//  Risky-0706012110052-AFL3
+//
+//  Created by MacBook Pro on 22/04/23.
+//
+
+//framework untuk menggambar grafis 2D pada ios
+import CoreGraphics
+
+struct HexagonParameters {
+    struct Segment {
+        //titik awal segmen dan titik akhir garis lurus
+        let line: CGPoint
+        //titik akhir segmen dan titik akhir kurva
+        let curve: CGPoint
+        //titik kontrol untuk menentukanbentuk kurva
+        let control: CGPoint
+    }
+
+    static let adjustment: CGFloat = 0.085
+
+    static let segments = [
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+        )
+    ]
+}
---
+++ Risky-0706012110052-AFL3/RotateBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotateBadgeSymbol.swift
+//  Risky-0706012110052-AFL3
+//
+//  Created by MacBook Pro on 22/04/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
+
