8f7816087f0a5959bb88771f2714ca6b158a38a8
Aerossky <riskyradeonrx560@gmail.com>
2023-04-22 15:20:20 +0700

    chapter 5 section 2

---
+++ Risky-0706012110052-AFL3/Views/Hikes/HikeView.swift
@@ -0,0 +1,57 @@
+//
+//  HikeView.swift
+//  Risky-0706012110052-AFL3
+//
+//  Created by MacBook Pro on 22/04/23.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+
+    var body: some View {
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.headline)
+                    Text(hike.distanceText)
+                }
+
+                Spacer()
+
+                Button {
+                    showDetail.toggle()
+                } label: {
+                    Label("Graph", systemImage: "chevron.right.circle")
+                        .labelStyle(.iconOnly)
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+//                        .animation(nil, value: showDetail)
+//                        .scaleEffect(showDetail ? 1.5 : 1)
+                        .padding()
+//                        .animation(.spring(), value: showDetail)
+                }
+            }
+
+            if showDetail {
+                HikeDetail(hike: hike)
+            }
+        }
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HikeView(hike: ModelData().hikes[0])
+                .padding()
+            Spacer()
+        }
+    }
+}
