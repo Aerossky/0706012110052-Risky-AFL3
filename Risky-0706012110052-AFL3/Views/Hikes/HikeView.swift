281eb74242ff3872a63523faddfae6ae186013bd
Aerossky <riskyradeonrx560@gmail.com>
2023-04-22 15:22:18 +0700

    chapter 5 section 3

--- Risky-0706012110052-AFL3/Views/Hikes/HikeView.swift
+++ Risky-0706012110052-AFL3/Views/Hikes/HikeView.swift
@@ -26,7 +26,9 @@ struct HikeView: View {
                 Spacer()
 
                 Button {
-                    showDetail.toggle()
+                    withAnimation(.easeInOut(duration: 1)){
+                        showDetail.toggle()
+                    }
                 } label: {
                     Label("Graph", systemImage: "chevron.right.circle")
                         .labelStyle(.iconOnly)
