43057a1338d57692cc0a2d03ea9efd41a3741211
Aerossky <riskyradeonrx560@gmail.com>
2023-04-24 19:42:44 +0700

    chapter 6 section 4

---
+++ Risky-0706012110052-AFL3/Views/CategoryItem.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryItem.swift
+//  Risky-0706012110052-AFL3
+//
+//  Created by MacBook Pro on 24/04/23.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
