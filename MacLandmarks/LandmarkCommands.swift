967dd79700911d5e3abef904f3b0d4c50cca7532
Aerossky <riskyradeonrx560@gmail.com>
2023-04-26 14:48:10 +0700

    chapter 10 section 5

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmarks
+//
+//  Created by MacBook Pro on 26/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
--- Risky-0706012110052-AFL3/Risky_0706012110052_AFL3App.swift
+++ Risky-0706012110052-AFL3/Risky_0706012110052_AFL3App.swift
@@ -13,10 +13,19 @@ struct Risky_0706012110052_AFL3App: App {
     @StateObject private var modelData = ModelData()
     
     var body: some Scene {
-            WindowGroup {
-                ContentView()
-                    .environmentObject(modelData)
-            }
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+        
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
+    
+        
+        
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
         #endif
